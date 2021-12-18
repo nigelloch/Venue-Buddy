5a1c55632450d137c25c1355fad86515afac8bce
loch <nigelloch@gmail.com>
2021-12-18 13:01:17 -0600

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,32 @@
 
 var searchBtn = document.querySelector("#search-btn")
+console.log("buttonworks")
 
 var zipEl = document.querySelector("#zip")
 
-var apiKey = "56247a0e9b7f734b3c0cf12d84afedc1ef3671e16480063c1abb4c8f8ddda557"
+//var apiKey = "56247a0e9b7f734b3c0cf12d84afedc1ef3671e16480063c1abb4c8f8ddda557"
+var apiKey = 'MjQ5NjgyNzZ8MTYzOTcwODEzMS4yNTQzNDE0'
 
-var searchZip = function() {
+var searchZip = function(e) {
+    e.preventDefault()
+    console.log(e)
 
     // var zip = zipEl.value
     // console.log(zip)
 
-    fetch("https://api.seatgeek.com/2/venues/632" + apiKey)
+    // venues api endpoint
+    fetch("https://api.seatgeek.com/2/venues/632/client_id=" + apiKey)
             .then(response => response.json())
             .then(data => {
                 console.log(data)
-                var state = document.querySelector("#state");
+                var state = document.querySelector("state");
                     state.textContent = state
 
 })
 }
+
+Request URL: https://api.seatgeek.com/2/venues/632/client_id=MjQ5NjgyNzZ8MTYzOTcwODEzMS4yNTQzNDE0
+
 
 
 
